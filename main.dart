577683ad6976066ce4b6820963165b9219a8bbbb
Zain Raza <zrabbas110@gmail.com>
2022-11-25 13:17:36 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,169 @@
+ import 'dart:html';
+
+void main(List<String> args) {
+  
+
+
+// Question 1
+
+
+// int length=20;
+// int breadth =20;
+// if(length==breadth)
+// {
+//   print("square values");
+// }
+// else
+// {
+//   print("rectangle values");
+// }
+
+
+
+
+// Question 2
+// int age1=18;
+// int age2=16;
+// int age3=55;
+// if(age1<age2 && age1<age3)
+// {
+//   print("age 1 is youngest");
+// }
+
+// else if(age2<age1 && age2<age3)
+// {
+//     print("age 2 is youngest");
+
+// }
+
+
+// else if(age3<age1 && age3<age2)
+// {
+//     print("age 3 is youngest");
+
+// }
+
+// if(age1>age2 && age1>age3)
+// {
+//   print("age 1 is oldest");
+// }
+// else if(age2>age1 && age2>age3){
+//  print("age 2 is oldest");
+// }
+
+// else if(age3>age1 && age3>age2){
+//  print("age 3 is oldest");
+// }
+
+
+
+
+// int Numberofclasses=16;
+// int Numberofclassesattended=10;
+// double percentage=Numberofclassesattended/Numberofclasses*100;
+// if(percentage>=75)
+// {
+//   print("You are not allowed to sit in the Exam");
+// }
+
+// else
+// {
+//   print("You are allowed to sit in the exam");
+// }
+
+
+//Question 4
+
+// int year=2000;
+// if(year%4==0)
+// {
+//      print("It is a Leap Year");
+
+// }
+// else
+// {
+//        print("It is not a Leap Year");
+// }
+
+
+// Question 5
+
+// int temperature=42;
+// if(temperature<0)
+// {
+//   print("Freezing weather");
+
+// }
+
+// else if(temperature>=0 && temperature<=10)
+// {
+//   print(" Very cold weather");
+// }
+
+// else if(temperature>=11 && temperature<=20)
+// {
+//   print("cold weather");
+// }
+
+// else if(temperature>=21 && temperature<=30)
+// {
+//   print(" Normal in Temp");
+// }
+// else if(temperature>=31 && temperature<=40)
+// {
+//   print("Its Hot");
+// }
+// else if(temperature>=40 )
+// {
+//   print("Its Very Hot");
+// }
+
+
+
+// question 6
+// var check = "a" ;
+  
+//   if(check == "a" || check == "e" || check == "i" || check =="o" || check =="u"){
+//     print("IT is a vowel");
+//   }
+//   else{
+//     print("Its a Consunant");
+//   }
+
+
+// Question 7
+
+
+// int id=1001;
+// String name="James";
+// int units=800;
+// var charge;
+// if(units <= 199)
+// {
+//   charge=units * 1.20;
+// }
+
+// else if(units <=200 && units>=400)
+// {
+// charge=units * 1.50;
+// }
+
+// else if(units <=401 && units>=600)
+// {
+//  charge=units * 1.80;
+// }
+
+// else if(units>=600)
+// {
+//    charge=units * 2.00;
+// }
+
+//  print("Customer Name : $name");
+//  print("Customer id : $id");
+//    print('Units Consumed $units');
+//    print("Amount Charges : $charge.00");
+//     print("Net BillPayment :  ${charge +240}.00");
+
+  }
+
+   
